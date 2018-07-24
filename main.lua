07e90c7b33c5fb3a6e6075cd7edaf9d0c801b23d
gopi <kgs10@iitbbs.ac.in>
2018-07-24 18:08:30 +0530

    "with dave player "

--- main.lua
+++ main.lua
@@ -1,40 +1,57 @@
 function love.load()
-    world = love.physics.newWorld(0, 200, true)
-        world:setCallbacks(beginContact, endContact, preSolve, postSolve)
+    world = love.physics.newWorld(0, 1000, true)
+    world:setCallbacks(beginContact, endContact, preSolve, postSolve)
     width = 1270
     height = 720
+    velocity = 400
     love.window.setMode(width , height)
     
     wall = love.graphics.newImage("images/redTile.png")
     wallWidth = wall:getWidth()
     wallHeight = wall:getHeight()
-    wallScale = 1.5
+    wallScale = 2
 
-    dave = love.graphics.newImage("images/dave1.png")
-    daveWidth = dave:getWidth()
-    daveHeight = dave:getHeight()
+    davePic = love.graphics.newImage("images/dave1.png")
+    daveScale = 2
+    daveWidth = davePic:getWidth()*daveScale
+    daveHeight = davePic:getHeight()*daveScale
 
     static = {}
         static.b = love.physics.newBody(world, width/2,height-wallHeight*wallScale/2, "static")
         static.s = love.physics.newRectangleShape(width,wallHeight*wallScale)
         static.f = love.physics.newFixture(static.b, static.s)
         static.f:setUserData("Block")
+    box = {}
+        box.b = love.physics.newBody(world, width/2,height-wallHeight*wallScale/2, "static")
+        box.s = love.physics.newRectangleShape(width,wallHeight*wallScale)
+        box.f = love.physics.newFixture(static.b, static.s)
+        box.f:setUserData("Block")
     daveCollider = {}
-        daveCollider.b = love.physics.newBody(world, 400,200, "dynamic")
+        daveCollider.b = love.physics.newBody(world, 400,600, "dynamic")
         daveCollider.b:setMass(10)
         daveCollider.s = love.physics.newRectangleShape(daveWidth,daveHeight)
         daveCollider.f = love.physics.newFixture(daveCollider.b, daveCollider.s)
         daveCollider.f:setRestitution(0)    -- make it bouncy
-        daveCollider.f:setUserData("daveCollider")
+        daveCollider.f:setUserData("Dave")
 end
 
 function love.update(dt)
     world:update(dt)
+    if love.keyboard.isDown("up") then
+    	if touch == 1 then
+    		touch = 0
+        	daveCollider.b:applyForce(0,-11000)
+        end
+    elseif love.keyboard.isDown("right") then
+        daveCollider.b:setX(daveCollider.b:getX()+velocity*dt)
+    elseif love.keyboard.isDown("left") then
+        daveCollider.b:setX(daveCollider.b:getX()-velocity*dt)
+    end
 end
 
 function love.draw()
-    love.graphics.polygon("line", daveCollider.b:getWorldPoints(daveCollider.s:getPoints()))
-    love.graphics.draw(dave,daveCollider.b:getX(),daveCollider.b:getX(),0 ,1 , 1 , 0 , 0)
+    --love.graphics.polygon("line", daveCollider.b:getWorldPoints(daveCollider.s:getPoints()))
+    love.graphics.draw(davePic, daveCollider.b:getX()-daveWidth/2,daveCollider.b:getY()-daveHeight/2, 0,daveScale,daveScale,0,0)
     for x = 0 , width/(wallScale*wallWidth) do
         love.graphics.draw(wall,x*wallScale*wallWidth,0,0,wallScale,wallScale,0,0)
         love.graphics.draw(wall,x*wallScale*wallWidth,height-wallScale*wallHeight,0,wallScale,wallScale,0,0)
@@ -44,4 +61,17 @@ function love.draw()
         love.graphics.draw(wall,width-wallScale*wallWidth,x*wallHeight*wallScale,0,wallScale,wallScale,0,0)
     end
     love.graphics.polygon("line", static.b:getWorldPoints(static.s:getPoints()))
+end
+
+function beginContact(a, b, coll)
+end
+ 
+function endContact(a, b, coll)
+end
+ 
+function preSolve(a, b, coll)
+end
+ 
+function postSolve(a, b, coll, normalimpulse, tangentimpulse)
+	touch =1
 end
